319fb37d381ab50c858e61e349232951a0fcd56d
IkerLabarga <ikerlabargacuadra@gmal.com>
2023-02-22 10:20:25 +0100

    Proyecto_documentado

--- src/examen/Fecha.java
+++ src/examen/Fecha.java
@@ -1,8 +1,22 @@
 package examen;
+/**
+ * 
+ * @author dam107
+ * @version 1.0
+ */
 
 public class Fecha {
+	/**
+	 * Inicializamos la variable booleana fechaValida que nos indicara si la fecha introducida es valida.
+	 */
 	public static boolean fechaValida;
-
+	/**
+	 * Metodo que nos comprueba si la fecha es correcta o no
+	 * @param anio introducimos el año 
+	 * @param mes introducimos el mes 
+	 * @param dia introducimos el dia
+	 * @return devolvera true si la fecha es valida y false si no.
+	 */
 	public static boolean validarFecha(int anio, int mes, int dia) {
 		fechaValida = false;
 		if ((dia > 0) && (mes > 0) && (anio > 0)) {
